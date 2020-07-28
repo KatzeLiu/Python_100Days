1f7e30e6623691cfeb785296019ed1cfb2e0bd90
Katzzze <ljq19950104@icloud.com>
2020-07-28 03:31:44 +0200

    Create Fibonacci sequence.py

---
+++ Day5/Fibonacci sequence.py
@@ -0,0 +1,24 @@
+#Fibonacci sequence
+def Fib(n):
+	if n<0:
+		print("Incorrect")
+	elif n == 1:
+		return 1
+	elif n == 2:
+		return 1
+	else:
+		return Fib(n-1) + Fib(n-2)
+
+num = [1, 1]
+for i in range(3, 21):
+	num.append(Fib(i))
+
+print(num)
+
+
+
+
+
+
+
+
